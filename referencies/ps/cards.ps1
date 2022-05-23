b0964a541175496daccfd681a250092a31f79543
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2022-05-23 16:28:33 -0300

    Preparing scripts

---
+++ referencies/ps/cards.ps1
@@ -0,0 +1,35 @@
+class Cards {
+
+    [System.Collections.Generic.List[string]]$cards
+    [string]$name
+
+    Cards([string]$_name) {
+        $this.name = $_name
+        $this.cards = [System.Collections.Generic.List[string]]::new()
+    }
+
+    NewDeck() {
+        $_suits = "`u{2663}","`u{2666}","`u{2665}","`u{2660}"
+        $_values = 'A',2,3,4,5,6,7,8,9,10,'J','Q','K'
+        $_deck = foreach ($s in $_suits){ foreach ($v in $_values){ "$v$s"} }
+        $this | Update-List -Property cards -Add $_deck | Out-Null
+    }
+
+    Show() {
+        Write-Host
+        Write-Host $this.name ": " $this.cards[0..12]
+        if ($this.cards.count -gt 13) {
+            Write-Host (' ' * ($this.name.length+3)) $this.cards[13..25]
+        }
+        if ($this.cards.count -gt 26) {
+            Write-Host (' ' * ($this.name.length+3)) $this.cards[26..38]
+        }
+        if ($this.cards.count -gt 39) {
+            Write-Host (' ' * ($this.name.length+3)) $this.cards[39..51]
+        }
+    }
+
+    Shuffle() { $this.cards = Get-Random -InputObject $this.cards -Count 52 }
+
+    Sort() { $this.cards.Sort() }
+}
--- scripts/pda_management.ps1
+++ scripts/pda_management.ps1
@@ -1,26 +1,36 @@
 function pda{
     param(
         [parameter(ValueFromPipelineByPropertyName, HelpMessage = "Please, inform the notification type")][ValidateSet("nk","hl")][string]$Client,
-        [parameter(ValueFromPipelineByPropertyName, HelpMessage = "Please, inform the notification type")][ValidateSet("Admin","WMS","INV","label")][string]$Project
+        [parameter(ValueFromPipelineByPropertyName, HelpMessage = "Please, inform the notification type")][ValidateSet("Admin","WMS","INV","Etiquetas","Label","etq","apk","Mobile")][string]$Project
         )
-    if ($Client -eq "nk") {
-        if ($Project -eq "Admin" -or $Project -eq "admin" -or $Project -eq "ADMIN") {
+    if($Client -eq "nk"){
+        if($Project -eq "Admin" -or $Project -eq "admin" -or $Project -eq "ADMIN"){
             Stop-IISSite -Name NK.Front.Admin
             Expand-Archive -Path $env:USERPROFILE\Desktop\admin.zip -DestinationPath C:\inetpub\wwwroot\repos\NKStore\Cloud.Autentication.React -Force
             Remove-Item -Path $env:USERPROFILE\Desktop\admin.zip -Force
             Start-IISSite -Name NK.Front.Admin
         }
-        if ($Project -eq "WMS" -or $Project -eq "wms" -or $Project -eq "Wms") {
+        if($Project -eq "WMS" -or $Project -eq "wms" -or $Project -eq "Wms"){
             Stop-IISSite -Name NK.Front.WMS
             Expand-Archive -Path $env:USERPROFILE\Desktop\wms.zip -DestinationPath C:\inetpub\wwwroot\repos\NKStore\Cloud.WMS.React -Force
             Remove-Item -Path $env:USERPROFILE\Desktop\wms.zip -Force
             Start-IISSite -Name NK.Front.WMS
         }
     }
-    if ($Client -eq "hl") {
-        if ($Project -eq "Admin" -or $Project -eq "admin" -or $Project -eq "label") {
-            Expand-Archive -Path $env:USERPROFILE\Desktop\etiquetas.zip -DestinationPath C:\inetpub\wwwroot\repos\api\Cloud.Gateway.Api\api\Atualizacao\etiquetas\ -Force
-            Remove-Item -Path $env:USERPROFILE\Desktop\etiquetas.zip -Force
+    if($Client -eq "hl"){
+        if($Project -eq "Admin" -or $Project -eq "admin" -or $Project -eq "ADMIN"){
+            Stop-IISSite -Name NK.Front.Admin
+            Expand-Archive -Path $env:USERPROFILE\Desktop\admin.zip -DestinationPath C:\inetpub\wwwroot\repos\NKStore\Cloud.Autentication.React -Force
+            Remove-Item -Path $env:USERPROFILE\Desktop\admin.zip -Force
+            Start-IISSite -Name NK.Front.Admin
         }
+		if($Project -eq "Etiquetas" -or $Project -eq "Label" -or $Project -eq "etq"){
+            Expand-Archive -Path $env:USERPROFILE\Desktop\etiquetas.zip -DestinationPath C:\inetpub\wwwroot\repos\api\Cloud.Gateway.Api\api\Atualizacao\etiquetas\ -Force    
+			Remove-Item -Path $env:USERPROFILE\Desktop\etiquetas.zip -Force
+        }
+		if($Project -eq "mobile" -or $Project -eq "apk"){
+            copy-item $env:USERPROFILE\Desktop\*.apk -Destination C:\inetpub\wwwroot\repos\api\Cloud.Gateway.Api\api\Atualizacao\etiquetas\ -Force
+			Remove-Item -Path $env:USERPROFILE\Desktop\*.apk -Force
+        }		
     }
 }
